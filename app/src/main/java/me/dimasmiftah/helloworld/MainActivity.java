6b5fe7b933a59d5ffef79c67ed9462ff3e2c4412
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2021-03-30 20:02:17 +0700

    fresh start

--- app/src/main/java/me/dimasmiftah/helloworld/MainActivity.java
+++ app/src/main/java/me/dimasmiftah/helloworld/MainActivity.java
@@ -3,8 +3,6 @@ package me.dimasmiftah.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void onBtnClick(View view) {
-        TextView txtHello = findViewById(R.id.txtMessage);
-        txtHello.setText("Hello Sob!");
     }
 }
